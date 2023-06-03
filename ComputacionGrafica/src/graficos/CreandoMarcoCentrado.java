3c0fec9e1ed23df3411a91994a7f4b7bd82b6ffc
Diego Rincón <97266392+diegoarff@users.noreply.github.com>
2023-06-03 13:16:59 -0400

    Add files via upload

---
+++ ComputacionGrafica/src/graficos/CreandoMarcoCentrado.java
@@ -0,0 +1,32 @@
+package graficos;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
+
+//Diego Rincón C.I. 29.929.768
+
+public class CreandoMarcoCentrado {
+
+	public static void main(String[] args) {
+		
+		MarcoCentrado marco = new MarcoCentrado();
+		marco.setVisible(true);
+		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
+
+class MarcoCentrado extends JFrame {
+	public MarcoCentrado() {
+		Toolkit miPantalla = Toolkit.getDefaultToolkit();
+		Dimension tamanioPantalla = miPantalla.getScreenSize();
+		int altura = tamanioPantalla.height;
+		int ancho = tamanioPantalla.width;
+		
+		setSize(ancho/2, altura/2);
+		setLocation(ancho/4, altura/4);
+				
+	}
+}
---
+++ ComputacionGrafica/src/graficos/CreandoMarcos.java
@@ -0,0 +1,24 @@
+package graficos;
+import javax.swing.*;
+
+//Diego Rincón C.I. 29.929.768
+
+public class CreandoMarcos {
+
+	public static void main(String[] args) {
+		MiMarco marco1 = new MiMarco();
+		marco1.setVisible(true);
+		marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
+
+class MiMarco extends JFrame {
+	public MiMarco() {
+		setBounds(500,300,550,250);
+		setTitle("Mi primera ventana");
+//		setResizable(true);
+//		setExtendedState(JFrame.MAXIMIZED_BOTH);
+	}
+}
